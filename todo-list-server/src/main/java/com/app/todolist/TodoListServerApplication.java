f3619b2a63a3e74d08ae3621341fa060eb9640ca
hp <hp@LAPTOP-VMDKQCCS>
2025-02-15 14:00:50 +0530

    Project setup

---
+++ todo-list-server/src/main/java/com/app/todolist/TodoListServerApplication.java
@@ -0,0 +1,13 @@
+package com.app.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListServerApplication.class, args);
+	}
+
+}
